149ee275aa5fda1411e6da13a64eb4aa13c5ce33
mourad_br <mouradbarkouch@gmail.com>
2022-06-03 20:45:06 +0100

    passing params

--- Virtual_0/main/pages/views.py
+++ Virtual_0/main/pages/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     #return HttpResponse('Hello Django community!')
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html',{'name':'Mourad','age':23})
 
 def about(request):
     return HttpResponse('About page.')
