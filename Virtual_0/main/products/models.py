b9b3d5d19cc1baef34da750ad6efb6e73a1f6b92
mourad_br <mouradbarkouch@gmail.com>
2022-06-04 17:07:47 +0100

    add database to products_app

--- Virtual_0/main/products/models.py
+++ Virtual_0/main/products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    image = models.ImageField(upload_to='photos/%y/%m/%d')
+    active = models.BooleanField(default=True)
