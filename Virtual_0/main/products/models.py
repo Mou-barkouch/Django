507d8e17b0d5e927753200d8dbf3c1cb7301619f
mourad_br <mouradbarkouch@gmail.com>
2022-06-05 03:42:42 +0100

    add categy to the product the migrations

--- Virtual_0/main/products/models.py
+++ Virtual_0/main/products/models.py
@@ -1,13 +1,21 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
 
 class Product(models.Model):
+    x =  [
+        ("banana", "banana"),
+        ("apple", "Apple")
+    
+    ]
+
     name = models.CharField(max_length=100)
     content = models.TextField()
     price = models.DecimalField(max_digits=6,decimal_places=2)
     image = models.ImageField(upload_to='photos/%y/%m/%d')
     active = models.BooleanField(default=True)
+    category = models.CharField(max_length=50, null=True, blank=True,choices=x)
 
     def __str__(self):
         return self.name
