67d59eb32a39379395cf563f7c9c783f13e47539
mourad_br <mouradbarkouch@gmail.com>
2022-06-05 02:48:33 +0100

    create views for product.html and products.html

--- Virtual_0/main/products/views.py
+++ Virtual_0/main/products/views.py
@@ -4,7 +4,7 @@ from .models import Product
 # Create your views here.
 
 def products(request):
-    return render(request, 'products/products.html')
+    return render(request, 'products/products.html',{'pros':Product.objects.all()})
 
 def product(request):
-    return render(request, 'products/product.html',{'name':'Ahmed'})
+    return render(request, 'products/product.html')
