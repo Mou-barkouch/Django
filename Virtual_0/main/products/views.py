9327cdd84f2e77f0dc677dee65eef73d66d9d4c8
mourad_br <mouradbarkouch@gmail.com>
2022-06-05 17:38:11 +0100

    query set range & in & exect...

--- Virtual_0/main/products/views.py
+++ Virtual_0/main/products/views.py
@@ -9,6 +9,10 @@ def product(request):
 def products(request):
     return render(request, 'products/products.html',{'pro':Product.objects.all()})
 ### Product.objects.filter(price=12)
+### Product.objects.filter(price__exect=12)
+### Product.objects.filter(name__contains='a')
+### Product.objects.filter(price__in=[1,30,6])
+### Product.objects.filter(price__range=[1,30])
 ### Product.objects.order_by(price)
 ### srt(Product.objects.all().count())
 ### Product.objects.exclude(price=10)
